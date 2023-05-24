0106e13a185333bc187258cdd19fde2cdb21ae93
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-05-24 21:27:04 +0600

    data added

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
+
 app.get('/', (req, res) => {
     res.send('app running');
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = recipes.find(recipe => recipe.id === id);
+    res.send(chef);
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
 });
 
 app.listen(port, () => {
