a81834d8ea286bebb87f90c4ed034e38e3df6d78
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-05-27 06:47:44 +0600

    recipe json added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
 const recipes = require('./data/recipes.json');
+const recipe = require('./data/recipe.json');
 
 app.get('/', (req, res) => {
     res.send('app running');
@@ -25,6 +26,16 @@ app.get('/chef/:id', (req, res) => {
 app.get('/recipes', (req, res) => {
     res.send(recipes);
 });
+
+app.get('/recipe', (req, res) => {
+    res.send(recipe);
+});
+
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    const singleRecipe = recipe.find(r => r.id === id);
+    res.send(singleRecipe);
+})
 
 app.listen(port, () => {
     console.log(`listening at port: ${port}}`);
